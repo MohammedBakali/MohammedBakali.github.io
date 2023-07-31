d5b53112b4dad0fdd0565e20436060516eaf4fa3
MohammedBakali <97497441+MohammedBakali@users.noreply.github.com>
2023-07-31 15:58:27 +0530

    PWA last ios changes part 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9a024de34deaf6dd8a51fcb4cb918879",
-"index.html": "c8252dc51ebf3439c554dc7f87dfbe40",
-"/": "c8252dc51ebf3439c554dc7f87dfbe40",
-"main.dart.js": "f88b013701a947369f342df30d0ba226",
+"index.html": "9f7414849aabba50a44c3460e4cd93d1",
+"/": "9f7414849aabba50a44c3460e4cd93d1",
+"main.dart.js": "a7328802c143d5ec33703de6771aec0d",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "mediSoft.png": "90641649d977fa2ce909ddc92948db31",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
