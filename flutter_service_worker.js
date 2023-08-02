37933cb6f628e77e7b8240402c305532417a0d5d
MohammedBakali <97497441+MohammedBakali@users.noreply.github.com>
2023-08-02 10:59:18 +0530

    IOS device ID get Part 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9a024de34deaf6dd8a51fcb4cb918879",
-"index.html": "66add04a0b04cb6304f588933fe9b642",
-"/": "66add04a0b04cb6304f588933fe9b642",
-"main.dart.js": "604c4f381c456bd08fcc1a673a60482d",
+"index.html": "08b379f66b5952730aa9477fb336c451",
+"/": "08b379f66b5952730aa9477fb336c451",
+"main.dart.js": "8aabd9488d3b46cb17245bfb0f43c728",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "mediSoft.png": "90641649d977fa2ce909ddc92948db31",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
